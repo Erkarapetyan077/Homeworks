6712e406b757cf2e0dde48f1a039e59f7d95db82
Erkarapetyan077 <erik5karapetyan@gmail.com>
2026-02-25 17:04:02 +0400

    new file

---
+++ script.js
@@ -0,0 +1,72 @@
+/*let x = 5.6789;
+console.log(x.toFixed(2));
+console.log(x);
+let y = Number(x.toFixed(2));
+console.log(y);
+console.log(typeof(y));
+console.log(typeof(x.toFixed(2)));*/
+/*function printAmount() {
+    console.log( amount.toFixed( 2 ) );
+}
+
+var amount = 99.99;
+printAmount(); // "99.99"
+
+amount = amount * 2;
+printAmount(); // "199.98"*/
+
+
+// 
+
+// function outer() {
+//     var a = 1;
+
+//     function inner() {
+//         var b = 2;
+//         // մենք այստեղ կարող ենք մուտք գործել և՛ `a`-ին, և՛ `b`-ին
+//         console.log( a + b ); // 3
+//     }
+
+//     inner();
+
+//     // մենք այստեղ կարող ենք մուտք գործել միայն `a`-ին
+//     console.log( a ); // 1
+// }
+
+// 
+
+// let a = 20;
+// console.log(typeof a);
+// let b = 30;
+// console.log(typeof String(b));
+
+// var a = "hello";
+// var b = a * 1;
+// console.log(b);
+
+// console.log(x);
+// var x = 10;
+
+// function foo() {
+//     var a = 1;
+
+//     if (a >= 1) {
+//         let b = 2;
+
+//         while (b < 5) {
+//             let c = b * 2;
+//             b++;
+
+//             console.log( a + c );
+//         }
+//     }
+// }
+
+// foo();
+
+// let a = Number.MAX_VALUE
+// // console.log(a);
+// // console.log(a + 1);
+// console.log(a * 5);
+let a = function(){};
+typeof(a);
