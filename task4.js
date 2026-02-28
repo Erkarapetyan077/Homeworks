cd743571524f9617081f0708f404364d23fbb2ad
Erkarapetyan077 <erik5karapetyan@gmail.com>
2026-02-28 19:30:41 +0400

    remove

--- task4.js
+++
@@ -1,9 +0,0 @@
-//4
-let str = "hello";
-let reverse = "";
-
-for(let i = str.length-1;i>=0;i--){
-
-    reverse+=str[i];
-}
-console.log(reverse);
--- task5.js
+++
@@ -1,4 +0,0 @@
-//5
-let str = "Learning JavaScript";
-
-console.log(str.includes("Java"));
