52e273536cf3fd723c947a84f63cb71ec23d5167
Erkarapetyan077 <erik5karapetyan@gmail.com>
2026-03-05 00:39:41 +0400

    new homework

--- 010326/task4.js
+++ 010326/task4.js
@@ -2,12 +2,13 @@ function countDigits(n) {
 
     n = Math.abs(n);
     let count = 0;
-    if(n === 0) return 1;
+    if (n === 0) return 1;
 
     while (n > 0) {
 
         n = Math.floor(n / 10);
-       // n = n / 10 >> 0;
+        // n = n / 10 >> 0;
+
         count++;
 
     }
