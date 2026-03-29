0c727ee2cfe3293b503598603dfee053404bb14f
Erkarapetyan077 <erik5karapetyan@gmail.com>
2026-03-29 21:35:34 +0400

    new homeworks

--- 030426/task.js
+++ 030426/task.js
@@ -240,7 +240,6 @@ const book1 = new Book({title:"Harry Potter", author:"J. K. Rowling", year:1997}
 const book2 = new Book({title:"1984", author:"George Orwell",year: 1949});
 const book3 = new Book({title:"Animal Farm",author: "George Orwell",year: 1945});
 const book4 = new Book({title:"The Hobbit", author:"J. R. R. Tolkien", year:1937});
-const book5 = new Book({title:"HHH",author:"J. R. R. Tolkien", year:1937});
 
 
 const library = new Library();
@@ -251,73 +250,47 @@ library.addBook(book3);
 library.addBook(book4);
 
 
-// console.log("=== All books ===");
-// library.showAllBooks();
+console.log("=== All books ===");
+library.showAllBooks();
 
-// console.log("=== Count books ===");
-// console.log(library.countBooks()); // 4
+console.log("=== Count books ===");
+console.log(library.countBooks()); // 4
 
-// console.log("=== Count available books ===");
-// console.log(library.countAvailableBooks()); // 4
+console.log("=== Count available books ===");
+console.log(library.countAvailableBooks()); // 4
 
-// console.log("=== Find by title ===");
-// console.log(library.findBookByTitle("1984"));
+console.log("=== Find by title ===");
+console.log(library.findBookByTitle("1984"));
 
-// console.log("=== Find by author ===");
-// console.log(library.findBooksByAuthor("George Orwell"));
+console.log("=== Find by author ===");
+console.log(library.findBooksByAuthor("George Orwell"));
 
-// console.log("=== Search books ===");
-// console.log(library.searchBooks("Harry"));
+console.log("=== Search books ===");
+console.log(library.searchBooks("Harry"));
 
-// console.log("=== Borrow book ===");
-// library.borrowBook("1984");
-// console.log(library.findBookByTitle("1984"));
+console.log("=== Borrow book ===");
+library.borrowBook("1984");
+console.log(library.findBookByTitle("1984"));
 
-// console.log("=== Borrow same book again ===");
-// library.borrowBook("1984");
+console.log("=== Borrow same book again ===");
+library.borrowBook("1984");
 
-// console.log("=== Return book ===");
-// library.returnBook("1984");
-// console.log(library.findBookByTitle("1984"));
+console.log("=== Return book ===");
+library.returnBook("1984");
+console.log(library.findBookByTitle("1984"));
 
-// console.log("=== Available books ===");
-// console.log(library.getAvailableBooks());
+console.log("=== Available books ===");
+console.log(library.getAvailableBooks());
 
-// console.log("=== Oldest book ===");
-// console.log(library.getOldestBook());
+console.log("=== Oldest book ===");
+console.log(library.getOldestBook());
 
-// console.log("=== Remove book ===");
-// library.removeBook("The Hobbit");
-// console.log(library.countBooks()); // 3
+console.log("=== Remove book ===");
+library.removeBook("The Hobbit");
+console.log(library.countBooks()); // 3
 
-// console.log("=== Final books ===");
-// library.showAllBooks();
-
-
-// let book1 = new Book("Brother Axe", "Hovhannes Tumanyan", 1907);
-// let book2 = new Book("shunn u katun Axe", "Hovhannes Tumanyan", 1902);
-// let book3 = new Book("dzaxort panosy ", "Hovhannes Tumanyan", 1889);
-// let library = new Library("Xnko apor");
-
-// library.addBook(book1);
-// library.addBook(book2);
-// library.addBook(book3);
-
-//console.log(library.removeBook("Brother Axee"));
-// console.log(library.findBookByTitle("Brother Axeu"));
-// console.log(library.findBooksByAuthor("Hovhannes Tumanyan"));
-// console.log(library.getAvailableBooks());
-// console.log(library.borrowBook("Brother Axe"));
-// console.log(library.returnBook("Brother Axe"));
-// console.log(library.showAllBooks());
-// console.log(library.countBooks());
-// console.log(library.countAvailableBooks());
-// console.log(library.searchBooks("Axe"));
-// console.log(library.getOldestBook());
-
-
-
-
+console.log("=== Final books ===");
+library.showAllBooks();
 
 
 
